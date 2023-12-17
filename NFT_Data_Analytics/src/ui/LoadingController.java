f0d9874fdf0fca5e31ca63e4e613fa4aef6eb048
DaoMinhNhat <daominhnhat0403@gmail.com>
2023-12-17 21:40:56 +0700

    remove progressBar

--- NFT_Data_Analytics/src/ui/LoadingController.java
+++ NFT_Data_Analytics/src/ui/LoadingController.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.scene.image.Image;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -12,6 +14,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -25,8 +28,11 @@ import marketplace.crawl.exception.InternetConnectionException;
 import marketplace.handler.MarketplaceHandler;
 
 public class LoadingController implements Initializable{
+//	@FXML
+//	private ProgressBar myProgressBar;
+	
 	@FXML
-	private ProgressBar myProgressBar;
+    private ImageView gifLoading;
 	
 	@FXML
 	private Label percentLabel;
@@ -38,33 +44,38 @@ public class LoadingController implements Initializable{
 	private Scene scene;
 	private Parent root;
 	
-    private Timeline timeline;
+//    private Timeline timeline;
     
 	IMarketplace m = new MarketplaceHandler();
 
-	public void increaseProgress() {
-		double increment = 0.1;
-        myProgressBar.setProgress(Math.min(1.0, myProgressBar.getProgress() + increment));
-        percentLabel.setText(String.format("%d%%", (int) Math.round(myProgressBar.getProgress() * 100)));
-        if (myProgressBar.getProgress() >= 1.0) {
-            timeline.stop();
-            loadingLabel.setText("Done");
-        }
-	}
+//	public void increaseProgress() {
+//		double increment = 0.1;
+//        myProgressBar.setProgress(Math.min(1.0, myProgressBar.getProgress() + increment));
+//        percentLabel.setText(String.format("%d%%", (int) Math.round(myProgressBar.getProgress() * 100)));
+//        if (myProgressBar.getProgress() >= 1.0) {
+//            timeline.stop();
+//            loadingLabel.setText("Done");
+//        }
+//	}
 	
 	public void clickBtnCrawl(ActionEvent event) {
 		ICrawlerManager crawlData = new CrawlerManager();
 
 		// startAutoIncrease
-        timeline = new Timeline(
-                new KeyFrame(Duration.seconds(0.5), e -> increaseProgress())
-        );
-        timeline.setCycleCount(Timeline.INDEFINITE);
-        timeline.play();
+//        timeline = new Timeline(
+//                new KeyFrame(Duration.seconds(0.5), e -> increaseProgress())
+//        );
+//        timeline.setCycleCount(Timeline.INDEFINITE);
+//        timeline.play();
+		
 
 		try {
 			m.clearData();
 			crawlData.crawlAllTrending();
+//			Image image = new Image(getClass().getResourceAsStream("/img/done.jpg"));
+//	        gifLoading.setImage(image);
+			
+			loadingLabel.setText("Done");
 		} catch (CrawlTimeoutException e) {
 			System.out.println(e.getMessage());
 		} catch (InternetConnectionException e) {
@@ -87,6 +98,6 @@ public class LoadingController implements Initializable{
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-	    myProgressBar.setStyle("-fx-accent: #00FF00;");
+//	    myProgressBar.setStyle("-fx-accent: #00FF00;");
 	}
 }
