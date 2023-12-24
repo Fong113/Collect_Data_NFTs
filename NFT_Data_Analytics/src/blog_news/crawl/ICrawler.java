8135d2edb4d45a1e0f7133e00813a10eb5cf826c
blackcat2005 <48479431+blackcat2005@users.noreply.github.com>
2023-12-25 00:33:47 +0700

    Final

--- NFT_Data_Analytics/src/blog_news/crawl/ICrawler.java
+++
@@ -1,9 +0,0 @@
-package blog_news.crawl;
-
-import java.io.IOException;
-
-import org.openqa.selenium.TimeoutException;
-
-public interface ICrawler {
-	void crawl() throws IOException, TimeoutException, RuntimeException;
-}
